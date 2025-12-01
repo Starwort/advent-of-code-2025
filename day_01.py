d367b548dc1e3e35fb1340fc78f59ff700c6abe0
starwort <tcphone93@gmail.com>
2025-12-01 05:06:06 +0000

    Day 1

--- day_01.py
+++ day_01.py
@@ -25,7 +25,9 @@ raw = aoc_helper.fetch(1, 2025)
 
 
 def parse_raw(raw: str):
-    return ...
+    return list(raw.splitlines()).mapped(
+        lambda line: (line[0] == "R", extract_ints(line)[0])
+    )
 
 
 data = parse_raw(raw)
@@ -35,7 +37,17 @@ data = parse_raw(raw)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    pos = 50
+    password = 0
+    for right, steps in data:
+        if right:
+            pos += steps
+        else:
+            pos -= steps
+        pos %= 100
+        if pos == 0:
+            password += 1
+    return password
 
 
 aoc_helper.lazy_test(day=1, year=2025, parse=parse_raw, solution=part_one)
@@ -45,10 +57,37 @@ aoc_helper.lazy_test(day=1, year=2025, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    pos = 50
+    password = 0
+    for right, steps in data:
+        direction = 1 if right else -1
+        for _ in range(steps):
+            pos += direction
+            pos %= 100
+            if pos == 0:
+                password += 1
+    return password
 
 
-aoc_helper.lazy_test(day=1, year=2025, parse=parse_raw, solution=part_two)
+aoc_helper.lazy_test(
+    day=1,
+    year=2025,
+    parse=parse_raw,
+    solution=part_two,
+    test_data=(
+        """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82""",
+        6,
+    ),
+)
 
 aoc_helper.lazy_submit(day=1, year=2025, solution=part_one, data=data)
 aoc_helper.lazy_submit(day=1, year=2025, solution=part_two, data=data)
